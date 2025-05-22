fb7546aa35e1e828e9b0a9f1a97b9bfd59418426
Patrique Rosa <le_pick07@hotmail.com>
2025-05-22 09:35:59 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+
 @Entity
 @Table(name = "tb_game")
 public class Game {
@@ -15,27 +16,32 @@ public class Game {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String title;
 	
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	public Games () {
-		
-	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	
+	public Game(Long id, String title, Integer year, String genre, String platforms,Double score,  String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -73,12 +79,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatform(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -107,7 +121,7 @@ public class Game {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(genre, id, imgUrl, longDescription, platform, shortDescription, title, year);
+		return Objects.hash(genre, id, imgUrl, longDescription, platforms, shortDescription, title, year);
 	}
 
 	@Override
@@ -122,6 +136,8 @@ public class Game {
 		return Objects.equals(id, other.id);
 	}
 	
+
+
 	
 	
 }
